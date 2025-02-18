3977a77d2569a19d6dc0a60e19db18c28c5de06e
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2025-02-18 20:57:34 +0500

    Add javascript for game functinality

---
+++ script.js
@@ -0,0 +1,66 @@
+let cards = []
+let sum = 0;
+let hasBlackjack = false;
+let isAlive = false;
+
+let message = "";
+let messageEl = document.getElementById('message');
+let sumEl = document.getElementById('sum');
+let cardsEl = document.getElementById('cards');
+// 3. Grab ahold of the player-el paragraph store it in a variable called playerEl
+let playerEl = document.getElementById('player-id');
+
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+	name: "Nasir",
+	chips: 135
+}
+
+// 4. Render the player's name and chips in playerEl
+playerEl.textContent = player.name + ': $' + player.chips
+
+function getRandomNumber() {
+	return Math.floor(Math.random() * 13) + 1
+}
+
+function startGame() {
+	isAlive = true
+	let firstCard = getRandomNumber();
+	let secondCard = getRandomNumber();
+
+	cards.push(firstCard, secondCard)
+	sum = firstCard + secondCard
+
+	renderGame()
+}
+
+function renderGame() {
+	cardsEl.textContent = 'Cards: '
+	for (let i = 0; i < cards.length; i++) {
+		cardsEl.textContent += cards[i] + " "
+	}
+
+	sumEl.textContent = "Sum: " + sum
+	if (sum <= 20) {
+		message = "Do you want to draw a new card?"
+	} else if (sum === 21) {
+		message = "You've got Balckjack!"
+		hasBlackjack = true
+	} else {
+		message = "You're out of the game!"
+		isAlive = false
+		sumEl.textContent = " "
+		cardsEl.textContent = " "
+	} 
+	messageEl.textContent = message
+} 
+
+function newCard() {
+	// Only allow the player to get a new card if she  IS alive and does NOT have Blackjack
+	if (isAlive === true && hasBlackjack === false) {
+		let card = getRandomNumber()
+		cards.push(card)
+		sum += card
+		renderGame()
+	}
+}
